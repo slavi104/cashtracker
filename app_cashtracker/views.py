f3331e8c65fe3a1671ed7a55ef6a91f19a6f57ef
Stanislav Venkov <slavi104@gmaIl.com>
2015-05-05 17:59:54 +0300

    + login page + logo

--- app_cashtracker/views.py
+++ app_cashtracker/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+# models
+from .models import User
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the cashtracker index.")
+    users = User.objects.order_by('-id')
+    # output = ', '.join([u.first_name for u in users])
+    # return HttpResponse(output)
+
+    template = loader.get_template('app_cashtracker/index.html')
+    context = RequestContext(request, {
+        'users': users,
+    })
+    return HttpResponse(template.render(context))
