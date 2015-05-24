bfa4b6970b29fc90c04a4fc15add9234793b9849
Stanislav Venkov <slavi104@gmaIl.com>
2015-05-24 16:31:57 +0300

    finished template for add/edit categories

--- app_cashtracker/static/app_cashtracker/js/main.js
+++ app_cashtracker/static/app_cashtracker/js/main.js
@@ -2,7 +2,7 @@ App = {
 
 };
 
-App.settingsScript = function(argument) {
+App.settingsScript = function() {
     $('#menu_button').on('click', function(){
         if ($(this).data('showed_menu') == '0') {
             $('#settings_menu').show('slide',{direction:'left'}, 200);
@@ -11,5 +11,22 @@ App.settingsScript = function(argument) {
             $('#settings_menu').hide('slide',{direction:'left'}, 200);
             $(this).data('showed_menu', '0');
         };
+    });
+}
+
+App.categoryAddEdit = function() {
+    var step_category_counter = 0;
+    $('#add_subcategory').on('click', function(e){
+        e.preventDefault();  
+        step_category_counter++;
+        $(".subcategory-input-holder").first().clone().appendTo(".subcategory-holder");
+        $(".subcategory-input-holder").last().removeClass('hidden');
+        $('.delete_button').last().on('click', function(){
+            $(this).parent().parent().parent().remove();
+        });
+    });
+
+    $('.delete_button').on('click', function(){
+        $(this).parent().parent().parent().remove();
     });
 }
